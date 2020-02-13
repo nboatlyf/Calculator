3253cefd79371a8986f42fe6cd6040135aeeca87
nboatlyf <33668519+nboatlyf@users.noreply.github.com>
2020-02-13 15:06:55 +0000

    Add functionality to multiply 2 numbers.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,7 +6,16 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the calculator!");
+            Console.WriteLine("Welcome to the calculator! \n");
+
+            Console.WriteLine("Firstly let's multiply 2 numbers together.");
+            Console.Write("Please enter the first number to multiply: ");
+            double multiplicand1 = double.Parse(Console.ReadLine());
+            Console.Write("Please enter the second number to multiply: ");
+            double multiplicand2 = double.Parse(Console.ReadLine());
+            double product = multiplicand1 * multiplicand2;
+            Console.WriteLine(multiplicand1 + " x " + multiplicand2 + " = " + product);
+
             Console.ReadLine();
         }
     }
