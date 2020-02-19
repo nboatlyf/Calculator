1c8327e58cd20daf10f8cf3e24179943c9d6e865
nboatlyf <33668519+nboatlyf@users.noreply.github.com>
2020-02-19 14:01:42 +0000

    feat: Add all standard operators.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Calculator
 {
@@ -6,17 +7,58 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the calculator! \n");
+            Console.WriteLine("Welcome to the calculator!");
+            Console.WriteLine("==========================");
+            Console.WriteLine("\n");
 
-            Console.WriteLine("Firstly let's multiply 2 numbers together.");
-            Console.Write("Please enter the first number to multiply: ");
-            double multiplicand1 = double.Parse(Console.ReadLine());
-            Console.Write("Please enter the second number to multiply: ");
-            double multiplicand2 = double.Parse(Console.ReadLine());
-            double product = multiplicand1 * multiplicand2;
-            Console.WriteLine(multiplicand1 + " x " + multiplicand2 + " = " + product);
+            // Specify which operators can be used.
+            List<string> validOperators = new List<string>();
+            validOperators.Add("+");
+            validOperators.Add("-");
+            validOperators.Add("*");
+            validOperators.Add("/");
+            string validOperatorsListString = string.Join(", ", validOperators.ToArray());
 
-            Console.ReadLine();
+            // Ask the user to choose an operator from the specified list.
+            Console.Write($"Please enter one of the following operators: {validOperatorsListString}: ");
+            string userOperator = Console.ReadLine();
+            if (!validOperators.Contains(userOperator))
+            {
+                throw new ArgumentOutOfRangeException($"Invalid operator entered. Please use one of the following: {validOperatorsListString}.");
+            }
+
+            // Ask the user to enter the first operand.
+            Console.Write("Please enter the first number: ");
+            string argument1 = Console.ReadLine();
+            if (!double.TryParse(argument1, out double operand1))
+                throw new ArgumentOutOfRangeException("Invalid argument entered. Please enter a number.");
+
+            // Ask the user to enter the second operand.
+            Console.Write("Please enter the second number: ");
+            string argument2 = Console.ReadLine();
+            if (!double.TryParse(argument2, out double operand2))
+                throw new ArgumentOutOfRangeException("Invalid argument entered. Please enter a number.");
+
+            double expressionEvaluation = 0;
+
+            // Evaluate the expression formed using the operator and 2 operands entered by the user.
+            if (userOperator == "+")
+                expressionEvaluation = operand1 + operand2;
+
+            else if (userOperator == "-")
+                expressionEvaluation = operand1 - operand2;
+
+            else if (userOperator == "*")
+                expressionEvaluation = operand1 * operand2;
+
+            else if (userOperator == "/")
+                expressionEvaluation = operand1 / operand2;
+
+            // Output the calculation for the user to see.
+            string message = $"{operand1} {userOperator} {operand2} = {expressionEvaluation}";
+            Console.WriteLine(message);
+
+            Console.ReadKey();
         }
     }
 }
