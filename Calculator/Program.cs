420423ebf78a2dbadb6cfccc3e661e1a502abf82
nboatlyf <33668519+nboatlyf@users.noreply.github.com>
2020-03-19 15:00:55 +0000

    feat: enable multiple operands.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -9,53 +9,95 @@ namespace Calculator
         {
             Console.WriteLine("Welcome to the calculator!");
             Console.WriteLine("==========================");
-            Console.WriteLine("\n");
+            Console.WriteLine(Environment.NewLine);
 
             // Specify which operators can be used.
-            List<string> validOperators = new List<string>();
-            validOperators.Add("+");
-            validOperators.Add("-");
-            validOperators.Add("*");
-            validOperators.Add("/");
-            string validOperatorsListString = string.Join(", ", validOperators.ToArray());
+            var validOperators = new List<string>
+            {
+                 "+"
+                ,"-"
+                ,"*"
+                ,"/"
+            };
+            var validOperatorsListed= string.Join(", ", validOperators.ToArray());
 
             // Ask the user to choose an operator from the specified list.
-            Console.Write($"Please enter one of the following operators: {validOperatorsListString}: ");
-            string userOperator = Console.ReadLine();
-            if (!validOperators.Contains(userOperator))
+            Console.Write($"Please enter one of the following operators: {validOperatorsListed}: ");
+            var chosenOperator = Console.ReadLine();
+            if (!validOperators.Contains(chosenOperator))
             {
-                throw new ArgumentOutOfRangeException($"Invalid operator entered. Please use one of the following: {validOperatorsListString}.");
+                throw new ArgumentOutOfRangeException($"Invalid operator entered. Please use one of the following: {validOperatorsListed}.");
             }
 
-            // Ask the user to enter the first operand.
-            Console.Write("Please enter the first number: ");
-            string argument1 = Console.ReadLine();
-            if (!double.TryParse(argument1, out double operand1))
-                throw new ArgumentOutOfRangeException("Invalid argument entered. Please enter a number.");
+            // Ask the user to choose the number of operands (that the chosen operator will be applied to).
+            Console.Write($"Please enter how many operands you would like to include: ");
+            var numberOfOperandsArgument = Console.ReadLine();
+            if (!int.TryParse(numberOfOperandsArgument, out var numberOfOperands))
+                throw new ArgumentOutOfRangeException($"Invalid number of operands entered. Please enter a positive integer number.");
+            if (numberOfOperands <= 0)
+                throw new ArgumentOutOfRangeException($"Invalid number of operands entered. Please enter a positive integer number.");
 
-            // Ask the user to enter the second operand.
-            Console.Write("Please enter the second number: ");
-            string argument2 = Console.ReadLine();
-            if (!double.TryParse(argument2, out double operand2))
-                throw new ArgumentOutOfRangeException("Invalid argument entered. Please enter a number.");
+            // Ask the user to specify the operands (that the chosen operator will be applied to, in the order the operands were specified).
+            var arguments = new List<string> { };
+            var operands = new List<double> { };
+            for (var i = 0; i < numberOfOperands; i++)
+            {
+                Console.Write($"Please enter operand {i}: ");
+                var argument = Console.ReadLine();
+                if (!double.TryParse(argument, out var operand))
+                    throw new ArgumentOutOfRangeException("Invalid argument entered. Please enter a number.");
+                arguments.Add(argument);
+                operands.Add(operand);
+            }
 
-            double expressionEvaluation = 0;
+            // Calculate the answer by applying the chosen operator to each operand, in the order they were entered into the program.
+            var intermediateAnswer = operands[0];
 
-            // Evaluate the expression formed using the operator and 2 operands entered by the user.
-            if (userOperator == "+")
-                expressionEvaluation = operand1 + operand2;
+            if (chosenOperator == "+")
+            {
+                for (var i = 1; i < numberOfOperands; i++)
+                {
+                    intermediateAnswer += operands[i];
+                }
+            }
 
-            else if (userOperator == "-")
-                expressionEvaluation = operand1 - operand2;
+            else if (chosenOperator == "-")
+            {
+                for (var i = 1; i < numberOfOperands; i++)
+                {
+                    intermediateAnswer -= operands[i];
+                }
+            }
 
-            else if (userOperator == "*")
-                expressionEvaluation = operand1 * operand2;
+            else if (chosenOperator == "*")
+            {
+                for (var i = 1; i < numberOfOperands; i++)
+                {
+                    intermediateAnswer *= operands[i];
+                }
+            }
 
-            else if (userOperator == "/")
-                expressionEvaluation = operand1 / operand2;
+            else if (chosenOperator == "/")
+            {
+                for (var i = 1; i < numberOfOperands; i++)
+                {
+                    intermediateAnswer /= operands[i];
+                }
+            }
+
+            var answer = intermediateAnswer;
+
+            // Write out the expression as a string.
+
+            var answerExpressionWrittenOut = arguments[0];
+
+            for (var i = 1; i < numberOfOperands; i++)
+            {
+                answerExpressionWrittenOut = $"({answerExpressionWrittenOut} {chosenOperator} {arguments[i]})";
+            }
 
             // Output the calculation for the user to see.
-            string message = $"{operand1} {userOperator} {operand2} = {expressionEvaluation}";
+            var message = $"Answer = {answerExpressionWrittenOut} = {answer}";
             Console.WriteLine(message);
 
             Console.ReadKey();
